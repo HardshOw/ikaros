8bac29c1ca01f86519eb44ada4501f705b8b4c2e
Florent <florent.mondelice@gmail.com>
2017-12-22 22:45:04 +0100

    fix increment issue on wrn command, and deploy on HU

--- commands/Admin/Warn.js
+++ commands/Admin/Warn.js
@@ -53,7 +53,7 @@ module.exports = class Warn extends commando.Command {
 							fields: [
 								{
 									name: 'Warn No°',
-									value: `${i}`
+									value: `${i+1}`
 								},
 								{
 									name: "Auteur :",

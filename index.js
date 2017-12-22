27692b04cdb79b2a2af6904f219857722d0d33ed
Florent <florent.mondelice@gmail.com>
2017-12-22 02:10:02 +0100

    First deploy

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const levelup = require('levelup')
 const leveldown = require('leveldown')
 
 const client = new Commando.Client({
-	owner: logs.OWNER,
+	owner: process.env.BOT_OWNER,
 	commandPrefix: '?',
 	unknownCommandResponse: false,
 	disableEveryone: true
@@ -30,4 +30,4 @@ client.on('guildMemberAdd', member => {
 	channel.send(`Bonjour <@${member.id}> sur le serveur Hentai Univers, je t\'invite a nous indiquer ton age avec la commande \?age \"ton_age\" avant de recevoir plus d\'informations ˆˆ`);
 });
 
-client.login(logs.TOKEN);
+client.login(process.env.BOT_TOKEN);

d41e72e58cd3f185701fb265f506f7b74bbaa97e
Florent <florent.mondelice@gmail.com>
2017-12-24 04:02:47 +0100

    Push Stable updates, add Role and help features.

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const leveldown = require('leveldown')
 const logs = require('./logs.json')
 
 const client = new Commando.Client({
-	owner: logs.OWNER,
+	owner: process.env.BOT_OWNER,
 	commandPrefix: '?',
 	unknownCommandResponse: false,
 	disableEveryone: true
@@ -36,4 +36,4 @@ ${command}
 Si tu as 16 ans.`);
 });
 
-client.login(logs.TOKEN);
+client.login(process.env.BOT_TOKEN);
